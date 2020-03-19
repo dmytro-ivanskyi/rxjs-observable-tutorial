2b5ed3d38e60baaaaf991e9d040c15b5a59d08f0
Dmytro Ivanskyi <ivanskoy.d@gmail.com>
2020-03-19 16:05:33 +0200

    Messing with observable.subscribe

--- src/code.ts
+++ src/code.ts
@@ -1,1 +1,25 @@
-import * as Rx from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
+
+const observable = Observable.create((observer: any) => {
+    try {
+    observer.next('Hey quys!');
+    observer.next('How are you?');
+    observer.complete();
+    observer.next('This will not send');
+    } catch(err) {
+        observer.error(err)
+    }
+});
+
+observable.subscribe(
+    (x: any) => addItem(x),
+    (error: any) => addItem(error),
+    () => addItem('Completed')
+    );
+
+function addItem(val: any) {
+    const node = document.createElement('li');
+    const textnode = document.createTextNode(val);
+    node.appendChild(textnode);
+    document.getElementById('output').appendChild(node);
+}
